078447fffba84b42eea9685ad42b4ee708b3956f
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-11-13 21:31:54 +0530

    Multiple Layer Neural Network

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -78,3 +78,26 @@ class Network(nn.Module):
         x = F.softmax(self.output(x), dim=1)
         
         return x
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Defining the layers, 128, 64, 10 units each
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        # Output layer, 10 units - one for each digit
+        self.fc3 = nn.Linear(64, 10)
+        
+    def forward(self, x):
+        ''' Forward pass through the network, returns the output logits '''
+        
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.softmax(x, dim=1)
+        
+        return x
+
+model = Network()
+model
