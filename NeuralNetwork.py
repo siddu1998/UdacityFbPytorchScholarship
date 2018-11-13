85a7393802bdbfffd7f759842b6746cd7f5e4cfe
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-11-13 21:22:03 +0530

    Functional help from PyTorch

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -60,3 +60,21 @@ class NeuralNetwork(nn.module):
         x = self.softmax(x)
         
         return x
+
+model = Network()
+model
+
+
+import torch.nn.functional as F
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(784, 256)
+        self.output = nn.Linear(256, 10)
+        
+    def forward(self, x):
+        x = F.sigmoid(self.hidden(x))
+        x = F.softmax(self.output(x), dim=1)
+        
+        return x
